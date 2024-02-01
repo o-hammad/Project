da4a087738f1044670bbf86eb1a25d987f23797e
Omar <ohammad@gwmail.gwu.edu>
2024-02-01 11:57:34 -0500

    express router error handling

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const cors = require('cors');
 const csurf = require('csurf');
 const { isProduction } = require('./config/keys');
 
+const debug = require('debug');
+
 const usersRouter = require('./routes/api/users'); // update the import file path
 const csrfRouter = require('./routes/api/csrf');
 
@@ -37,9 +39,31 @@ app.use(
     })
 );
 
-
 // Attach Express routers
 app.use('/api/users', usersRouter); // update the path
 app.use('/api/csrf', csrfRouter);
+
+// Express custom middleware for catching all unmatched requests and formatting
+// a 404 error to be sent as the response.
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.statusCode = 404;
+    next(err);
+});
+
+const serverErrorLogger = debug('backend:error');
+
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+    serverErrorLogger(err);
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        statusCode,
+        errors: err.errors
+    })
+});
 
 module.exports = app;
